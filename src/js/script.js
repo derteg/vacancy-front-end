a392dd4b46bbc4c56da9bd14c4441db4b0452a8a
derteg <i.ya.kadet@gmail.com>
2015-09-14 22:05:03 +0400

    ad
    desktop

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,38 @@
 $(function() {
 	var $cont = $('.js-promo-test'),
-		$label = $('label', $cont);
+		$label = $('label', $cont),
+		$knob = $(".js-dial");
+
+	$knob.knob({
+		width: '252',
+		height: '252',
+		fgColor: '#ffbe0f',
+		dynamicDraw: true,
+		lineCap: 'round',
+		thickness: '.02',
+		readOnly: true,
+		skin:'tron',
+		bgColor: '#5b4739',
+		displayInput:false
+	});
+
+	$knob.append(function() {
+		$knob.parent().parent().find('.b-result__circle-content').text(0+'%');
+	});
 
 	$label.click(function(){
 		var that = $(this),
-			valNew = that.data('test-proc'),
-			val = $(".js-dial").val();
-
-		if(that.prev().prop('checked') === false){
-			$(".js-dial").val(parseFloat(val) + parseFloat(valNew)).trigger('change');
+			myVal = $knob.val(),
+			valNew = that.data('test-proc');
+	
+		if(that.parent('.b-promo__test-row').find('input').prop('checked') === false){
+			$knob.val(parseFloat(myVal) + parseFloat(valNew)).trigger('change');
 		} else {
-			$(".js-dial").val(parseFloat(val) - parseFloat(valNew)).trigger('change');
+			$knob.val(parseFloat(myVal) - parseFloat(valNew)).trigger('change');
 		}
-	});
 
-	$(".js-dial").knob({
-		width: '252px',
-		fgColor: '#ffbe0f',
-		thickness: '.02',
-		readOnly: true,
-		inputColor: '#ffbe0f',
-		font: 'bebas_neuebold',
-		bgColor: '#5b4739'
+		$knob.append(function() {
+			$knob.parent().parent().find('.b-result__circle-content').text($knob.val() + '%');
+		});
 	});
 });
